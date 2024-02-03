641ce65322601971a42841c555f68896b70680ab
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-02-03 10:04:07 +0100

    remove handlebars

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
-import handebars from 'handlebars';
-import fs from 'fs/promises';
 
 const app = express();
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './templates');
 
 let fruitBoxes = [
   {
