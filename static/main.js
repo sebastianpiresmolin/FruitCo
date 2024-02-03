bef35c50ba29516f442b247fc37dfd2e68c47a86
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-02-03 17:43:54 +0100

    added comments

--- static/main.js
+++ static/main.js
@@ -1,24 +1,32 @@
-// Search by id or fruit name function
+// Declare an array to store the filtered boxes
 let filteredBoxes = [];
 
+// Define an asynchronous function to handle the search
 async function handleSearch() {
+  // Get the search value from the input field
   let searchValue = document.getElementById('searchInput').value;
 
+  // Get the result list element
   let resultList = document.getElementById('resultList');
+  // Remove all child nodes of the result list to clear previous results
   while (resultList.firstChild) {
     resultList.removeChild(resultList.firstChild);
   }
 
+  // Check if the search value is a number or a string
   if (isNaN(searchValue)) {
+    // If it's a string, fetch boxes that include this fruit
     const response = await fetch(`/getBoxesIncludingFruit/${searchValue}`);
     filteredBoxes = await response.json();
   } else {
+    // If it's a number, fetch boxes by this ID
     const response = await fetch(`/getAllById/${searchValue}`);
     filteredBoxes = await response.json();
   }
 
   // Check if filteredBoxes.boxes is an array and has elements
   if (Array.isArray(filteredBoxes.boxes) && filteredBoxes.boxes.length > 0) {
+    // If it does, create and append a list item for each box
     for (let i = 0; i < filteredBoxes.boxes.length; i++) {
       let li = document.createElement('li');
 
@@ -42,14 +50,19 @@ async function handleSearch() {
       document.getElementById('resultList').appendChild(li);
     }
   } else {
+    // If it doesn't, create and append a list item with the text 'No result'
     let li = document.createElement('li');
     li.textContent = 'No result';
     document.getElementById('resultList').appendChild(li);
   }
 
+  // Return the filtered boxes
   return filteredBoxes;
 }
 
+// Add an event listener for the DOMContentLoaded event
+// When the DOM is fully loaded, add a click event listener to the search button
+// When the search button is clicked, call the handleSearch function
 document.addEventListener('DOMContentLoaded', () => {
   document
     .getElementById('searchButton')
